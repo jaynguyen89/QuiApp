81625549004327b227f4e107516dae0fc0209e3f
Jay Nguyen <jay.nguyen@jaydeveloper.com>
2018-05-30 22:59:57 +1000

    reviewing concepts, changes not useful

---
+++ QuiApp/Models/Lecturers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuiApp.Models
+{
+    public partial class Lecturers
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+
+        public AspNetUsers User { get; set; }
+    }
+}
---
+++ QuiApp/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuiApp.Models
+{
+    public partial class Students
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public double? Mark { get; set; }
+        public bool IsActive { get; set; }
+
+        public AspNetUsers User { get; set; }
+    }
+}
